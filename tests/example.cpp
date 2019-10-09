16dfa809a934b1b5c39e7dc3c491f43fc99744e3
Jacob Turner <jacob.turner870@gmail.com>
2019-10-09 13:05:23 +0200

    First working version

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,36 +1,44 @@
 #include <contextual.h>
 #include <algorithm>
+#define eval [&](auto resource)
 
 struct Data {
 	std::string username;
 	std::string password;
 };
 
+//typedef Context BaseContext<Data>;
+
 class Resource : public BaseResource {
 public:
 	std::string _password = "xxxxxxxxx";
 
 	void enter() override {
+		
 		std::swap(_password, resources->password);
 	}
 
-	void exit(std::exception& e) override {
+	void exit(std::optional<std::exception> e) override {
 		std::swap(_password, resources->password);
+
 	}
 
 
-	Resource(Data& resources): BaseResource::BaseResource(resources){};
+	Resource(Data resources): BaseResource::BaseResource(&resources){};
 };
 
 
 int main(){
 	Data first_user{"AmbarDas", "hickory76"};
-	with PasswordHidden = Resource(first_user) + Context{
-		[&](Data* resource){
-			std::cout << "Username: " << resource->username << "\n";
-			std::cout << "Password: " << resource->password << "\n";
+	With {
+		Resource(first_user) + Context {
+			eval {
+				std::cout << "Username: " << resource->username << "\n";
+				std::cout << "Password: " << resource->password << "\n";
+			}
 		}
 	};
+
 
 	std::cout << "Username: " << first_user.username << "\n";
 	std::cout << "Password: " << first_user.password << "\n";
