ed556fff988877a5826d8a0f28d570a2839f4ba2
Jacob Turner <jacob.turner870@gmail.com>
2019-10-09 17:27:17 +0200

    Updated example to show that causing side effects on acquired resources is possible

--- tests/example.cpp
+++ tests/example.cpp
@@ -5,6 +5,7 @@
 struct Data {
 	std::string username;
 	std::string password;
+	bool logged_in = false;
 };
 
 //typedef Context BaseContext<Data>;
@@ -14,27 +15,29 @@ private:
 	std::string _password = "xxxxxxxxx";
 
 	void enter() override {
-		
 		std::swap(_password, resources->password);
 	}
 
 	void exit(std::optional<std::exception> e) override {
 		std::swap(_password, resources->password);
+		resources->logged_in = true;
 
 	}
 public:
 
-	Resource(Data resources): BaseResource::BaseResource(&resources){};
+	Resource(Data &resources): BaseResource::BaseResource(&resources){};
 };
 
 
 int main(){
 	Data first_user{"admin", "password123"};
+	std::cout << "Logged in: " << first_user.logged_in << "\n\n";
 	With {
 		Resource(first_user) + Context {
 			eval {
 				std::cout << "Username: " << resource->username << "\n";
 				std::cout << "Password: " << resource->password << "\n";
+				resource->logged_in = true;
 			}
 		}
 	};
@@ -42,4 +45,5 @@ int main(){
 
 	std::cout << "Username: " << first_user.username << "\n";
 	std::cout << "Password: " << first_user.password << "\n";
+	std::cout << "Logged in: " << first_user.logged_in << "\n";
 }
